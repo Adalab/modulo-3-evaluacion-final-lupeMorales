acb376f9044651fa9cb55e3a6a2fc7a064234e63
lupeMorales <morales.lupegmail.com>
2022-08-27 21:23:32 +0200

    add detailCard basic style

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -1,22 +1,31 @@
+import "../styles/components/CharacterDetails.scss";
 import { Link } from "react-router-dom";
 
 const CharacterDetails = (props) => {
   return (
-    <section>
+    <section className="detail">
       <Link to="/">
         <p>Volver</p>
       </Link>
-      <img
-        src={props.character.image}
-        alt={`Foto de ${props.character.name}`}
-        title={`Foto de ${props.character.name}`}
-      ></img>
-      <h3>{props.character.name}</h3>
-      <p>{props.character.specie}</p>
-      <p>casa:{props.character.house}</p>
-      <p>género{props.character.gender}</p>
-      <p>ancestro{props.character.ancestry}</p>
-      <p>{props.character.status}</p>
+      <article className="detail__container">
+        <div>
+          <img
+            src={props.character.image}
+            alt={`Foto de ${props.character.name}`}
+            title={`Foto de ${props.character.name}`}
+          ></img>
+        </div>
+
+        <div detail__info>
+          <h3>{props.character.name}</h3>
+
+          <p>{props.character.specie}</p>
+          <p>casa:{props.character.house}</p>
+          <p>género{props.character.gender}</p>
+          <p>ancestro{props.character.ancestry}</p>
+          <p>{props.character.status}</p>
+        </div>
+      </article>
     </section>
   );
 };
